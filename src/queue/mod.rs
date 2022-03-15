70bceadf51602b621a3a5ca3294cab13c04bf616
austin <austinchang4@gmail.com>
2022-03-15 09:43:42 +0800

    Queue struct with new_task method; static singleton Queue

--- src/queue/mod.rs
+++ src/queue/mod.rs
@@ -1,5 +1,5 @@
 use crossbeam_channel::{Receiver, Sender};
-use std::{collections::HashMap, sync};
+use std::{collections::HashMap, lazy::SyncLazy, sync};
 
 use self::task::Task;
 
@@ -24,11 +24,40 @@ impl<'a> Queue<'a> {
         }
     }
 
-    pub fn add_task(&self, data: &Vec<u8>) -> String {
+    // Add new task to queue
+    pub fn add_task(
+        &mut self,
+        chan_name: &str,
+        data: &'a Vec<u8>,
+    ) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
         let task = task::Task::new(data);
-        let id = task.id.clone();
-        // let chan = self.ch.get(&id).unwrap().chan.clone();
-        // chan.0.send(task).unwrap();
-        id
+        // add task to queue
+        let x = match self.tasks.get_mut() {
+            Ok::<&mut Vec<Task<'a>>, _>(tasks) => {
+                let task_id = task.id.clone().to_owned();
+                tasks.push(task.clone());
+                task_id
+            }
+            Err(_) => return Err("Failed to add task".into()),
+        };
+        // send task through channels
+        let chan = self
+            .ch
+            .entry(chan_name.to_owned())
+            .or_insert_with(|| QueueChan {
+                id: chan_name.to_owned(),
+                chan: crossbeam_channel::unbounded(),
+                status: task::TaskStatus::Pending,
+            });
+        match chan.chan.0.send(task.clone()) {
+            Ok(_) => {}
+            Err(err) => return Err(format!("Failed to send task to channel|Err: {}", err).into()),
+        };
+
+        // success
+        Ok(x)
     }
 }
+
+// singleton Queue
+pub static Q: SyncLazy<Queue> = SyncLazy::new(|| Queue::new());
