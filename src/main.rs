418d0093e55733f01f56ab1bf267c55d4122a2c2
austin <austinchang4@gmail.com>
2022-03-20 19:10:20 +0800

    load dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 #![feature(once_cell)]
 
-use std::net::ToSocketAddrs;
+use dotenv::dotenv;
+use std::{env, net::ToSocketAddrs};
 
 mod grpc;
 mod queue;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    //
+    {
+        dotenv().ok();
+    }
     println!("Hello, world!");
 
     // gRPC server
     {
+        let port = env::var("PORT").unwrap_or("8080".to_owned());
         let miniq_server = grpc::miniq::MiniQServer {};
+        println!("Starting gRPC server on port {}", port); // debug
         tonic::transport::Server::builder()
             .add_service(grpc::miniq::mini_q::mini_q_server::MiniQServer::new(
                 miniq_server,
             ))
-            .serve("[::1]:8080".to_socket_addrs().unwrap().next().unwrap())
+            .serve(
+                format!("[::1]:{}", port)
+                    .to_socket_addrs()
+                    .unwrap()
+                    .next()
+                    .unwrap(),
+            )
             .await?;
     }
     Ok(())
