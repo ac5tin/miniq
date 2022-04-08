382d264ace23c68e4505e63bb747ff968d6c15da
austin <austinchang4@gmail.com>
2022-04-08 22:34:18 +0800

    feat: users can set HOST using environment variables

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     // gRPC server
     {
+        let host = env::var("HOST").unwrap_or("[::1]".to_string());
         let port = env::var("PORT").unwrap_or("8080".to_owned());
         let miniq_server = grpc::miniq::MiniQServer {};
-        println!("Starting gRPC server on port {}", port); // debug
+        println!("Starting gRPC server on  {}:{}", host, port); // debug
         tonic::transport::Server::builder()
             .add_service(grpc::miniq::mini_q::mini_q_server::MiniQServer::new(
                 miniq_server,
             ))
             .serve(
-                format!("[::1]:{}", port)
+                format!("{}:{}", host, port)
                     .to_socket_addrs()
                     .unwrap()
                     .next()
